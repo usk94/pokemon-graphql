046c1b7eb494b6b2759bc975e8c22df8560f2396
Yusaku <hitoyasablue@gmail.com>
2022-12-27 21:06:49 +0900

    fetch from graphql pokemon app

--- codegen.ts
+++ codegen.ts
@@ -3,12 +3,14 @@ import type { CodegenConfig } from "@graphql-codegen/cli"
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://graphql-pokemon2.vercel.app",
-  documents: "src/**/*.tsx",
+  documents: ["src/**/*.tsx", "pages/*.tsx"],
   ignoreNoDocuments: true,
   generates: {
-    "src/gql/": {
-      preset: "client",
-      plugins: [],
+    "./src/@types/types.d.ts": {
+      plugins: ["typescript", "typescript-operations"],
+    },
+    "./schema.graphql": {
+      plugins: ["schema-ast"],
     },
   },
 }
