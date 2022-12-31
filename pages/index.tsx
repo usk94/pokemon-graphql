66b013be6a58e29f023ebf70a7899c0270cbfd58
Yusaku <hitoyasablue@gmail.com>
2022-12-31 11:50:42 +0900

    english name to japanese name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
 import PokemonPagination from "../src/components/pokemonPagination"
 import styles from "../styles/PaginationContainer.module.scss"
+import csv from "csvtojson"
 
-const Component = () => {
+export type PokemonName = {
+  japanese: string
+  english: string
+}
+
+const Component = ({ nameJson }: { nameJson: PokemonName[] }) => {
   return (
     <>
       <div className={styles.container}>
-        <PokemonPagination itemsPerPage={10} />
+        <PokemonPagination itemsPerPage={10} nameJson={nameJson} />
       </div>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const csvFilePath = "./pokemon_name.csv"
+  const nameJson = await csv().fromFile(csvFilePath)
+  return { props: { nameJson } }
 }
 
 export default Component
